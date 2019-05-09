2c6004769b142eef8a1fb879793e9fb0c8abd29a
abourke09 <aabourke@gmail.com>
2019-05-09 14:54:25 -0700

    lists all possible routes in a general sense, option to refine them later

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :options
-  resources :players
+
+  namespace :api do
+
+    resources :games
+    resources :options
+    resources :players
+
+    resources :games do
+      resources :players
+      resources :options
+    end
+
+    resources :players do
+      resources :games
+      resources :options
+    end
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
