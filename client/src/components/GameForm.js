92f0889d74306dd85258ac05515d948b1062a36f
abourke09 <aabourke@gmail.com>
2019-05-16 16:59:41 -0700

    changes name of new game component

---
+++ client/src/components/GameForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GameForm = () => (
+  <div className="game form">
+    <h1>The new game form should be listed here</h1>
+  </div>
+  )
+
+export default GameForm;
--- client/src/components/New.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const New = () => (
-  <div className="New">
-    <h1>The New game forms should be listed here</h1>
-  </div>
-  )
-
-export default New;
