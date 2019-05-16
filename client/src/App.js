beaf38e8fe2510ca27d35851074a0db195df5309
abourke09 <aabourke@gmail.com>
2019-05-16 16:54:30 -0700

    changes name of components

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MyNavbar from './components/Navbar'
 import Home from './components/Home';
-import New from './components/New';
-import AllPlayers from './components/AllPlayers';
-import AllGames from './components/AllGames';
+import GameForm from './components/GameForm';
+import Players from './components/Players';
+import Games from './components/Games';
 
 function App() {
   return (
@@ -12,9 +12,9 @@ function App() {
       <Router>
         <MyNavbar/>
         <Route exact path="/" component={Home}/>
-        <Route exact path="/new" component={New}/>
-        <Route exact path="/allPlayers" component={AllPlayers}/>
-        <Route exact path="/allGames" component={AllGames}/>
+        <Route exact path="/games/new" component={GameForm}/>
+        <Route exact path="/players" component={Players}/>
+        <Route exact path="/games" component={Games}/>
       </Router>
     </div>
   );
