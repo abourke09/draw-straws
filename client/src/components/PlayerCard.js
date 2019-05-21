223c416819a976f737aca381820824e8eeb9f7cc
abourke09 <aabourke@gmail.com>
2019-05-21 15:44:05 -0700

    Adds style margin and flex to Card component in order to wrap cards within CardDeck and space them out. Removes commented-out portion of code.

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
-//import Nav from 'react-bootstrap/Nav'
 
 const PlayerCard = (props) => {
   const url = `/players/${props.player.id}`
@@ -8,27 +7,21 @@ const PlayerCard = (props) => {
   const gameNames = props.player.games.map( (game, index) => <li key="index">{game.name}</li> )
 
     return (
-      <Card bg="light" style={{ width: '18rem' }}>
-        <Card.Header as="h4">{props.player.name}</Card.Header>
+      <Card bg="light" style={ {margin: '25px', flex: '0 1 24%'} }>
+        <Card.Header>{props.player.name}</Card.Header>
+
         <Card.Body>
           <Card.Subtitle>Number of Games Played: {props.player.games.length}</Card.Subtitle>
+
           <Card.Text className="mb-2 text-muted">
             <ol>{gameNames}</ol>
           </Card.Text>
+
           <Card.Link href= {url} >View Player's Page</Card.Link>
         </Card.Body>
+
       </Card>
       )
 }
 
 export default PlayerCard;
-
-{/*  <div class="card" >
-    <h4>Name: {props.player.name}</h4>
-    <h4>Number of Games Played: {props.player.games.length}</h4>
-    <h4>{props.player.name} has played {props.player.games.length} game(s)</h4>
-    {props.player.games.map((game, index) =>
-      <p key={index}>{index + 1}. {game.name} </p>
-    )}
-    </div>
-*/}
