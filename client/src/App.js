af05c56efd5dc1bc22cfe0d4a8fc0e37a54f597a
abourke09 <aabourke@gmail.com>
2019-05-21 14:05:59 -0700

    imports React Component, connect to react-redux, fetchPlayers. Changes App function to App class component that fetches players once mounted.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MyNavbar from './components/Navbar'
 import Home from './components/Home';
 import GameForm from './components/GameForm';
 import Players from './components/Players';
 import Games from './components/Games';
+//import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux';
+import { fetchPlayers } from './actions/players'
 
-function App() {
-  return (
-    <div>
-      <Router>
-        <MyNavbar/>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/games/new" component={GameForm}/>
-        <Route exact path="/players" component={Players}/>
-        <Route exact path="/games" component={Games}/>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  componentDidMount(){
+    this.props.fetchPlayers()
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <MyNavbar/>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/games/new" component={GameForm}/>
+          <Route exact path="/players" component={Players}/>
+          <Route exact path="/games" component={Games}/>
+        </Router>
+      </div>
+    )
+  }
 }
 
+// const mapStateToProps = state => {
+//   return {
+//     players: state.players.all
+//   }
+// }
 
+// const mapDispatchToProps = dispatch => bindActionCreators({
+//   fetchPlayers
+// }, dispatch)
 
-export default App;
+export default connect(null, {fetchPlayers})(App)
