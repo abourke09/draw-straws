80716615122c26866ae26eaea52fd983d1ddc9c1
abourke09 <aabourke@gmail.com>
2019-05-21 17:20:34 -0700

    adds FETCH_GAMES case to gameReducer

--- client/src/reducers/gamesReducer.js
+++ client/src/reducers/gamesReducer.js
@@ -5,6 +5,8 @@ const initialState = {
 
 export default function gamesReducer(state = initialState, action) {
   switch(action.type) {
+    case 'FETCH_GAMES':
+      return { ...state, all: action.payload}
     case 'SET_GAME':
       return { ...state, current: action.payload }
     default: return state
