55120bc3a335cdaae757cbab78ae2bc132fb2c20
abourke09 <aabourke@gmail.com>
2019-05-22 11:47:54 -0700

    creates new file and inputs basic code

---
+++ client/src/components/GameShow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GameShow = () => (
+  <div className="game show">
+    <h1>The Game Show page should be here</h1>
+  </div>
+  )
+
+export default GameShow;
---
+++ client/src/components/PlayerShow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayerShow = () => (
+  <div className="player show">
+    <h1>The Player Show page should be here</h1>
+  </div>
+  )
+
+export default PlayerShow;
