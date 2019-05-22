b56d27746320e50a97931bb623b6b41bd0d8b6c4
abourke09 <aabourke@gmail.com>
2019-05-22 11:48:57 -0700

    experimenting with NavBar, no real edits

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,22 +2,21 @@ import React, {Component} from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
-
 class myNavbar extends Component {
   render() {
     return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand>Draw Straws</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/games/new">New</Nav.Link>
-            <Nav.Link href="/players">All Players</Nav.Link>
-            <Nav.Link href="/games">All Games</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>Draw Straws</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/games/new">New</Nav.Link>
+              <Nav.Link href="/players">All Players</Nav.Link>
+              <Nav.Link href="/games">All Games</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
     )
   }
 }
