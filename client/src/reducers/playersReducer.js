c537b124211ca293b7b282060a210a42baa293d0
abourke09 <aabourke@gmail.com>
2019-05-30 19:42:52 -0700

    adds a SET_PLAYER case to the playerReducer

--- client/src/reducers/playersReducer.js
+++ client/src/reducers/playersReducer.js
@@ -7,6 +7,8 @@ export default function playersReducer(state = initialState, action) {
   switch(action.type) {
     case 'FETCH_PLAYERS':
       return { ...state, all: action.payload }
+    case 'SET_PLAYER':
+      return { ...state, current: action.payload }
     default: return state
   }
 }
