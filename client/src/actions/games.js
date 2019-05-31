572a54e8667eeb000366ea87f6f135a8daec02e5
abourke09 <aabourke@gmail.com>
2019-05-30 19:41:28 -0700

    adds a setGame constant that GETS the json data from a particular Game and dispatches SET_GAME to the reducer to save that data to state.

--- client/src/actions/games.js
+++ client/src/actions/games.js
@@ -1,5 +1,6 @@
 const baseUrl = 'http://localhost:3001/api'
 
+//fetch ALL of the games
 export const fetchGames = () => {
   let data = {
     method: 'GET',
@@ -15,6 +16,27 @@ export const fetchGames = () => {
       .then(games => dispatch({
           type: 'FETCH_GAMES',
           payload: games
+      }))
+      .catch(err => err)
+  }
+}
+
+//fetch and set ONE particular game
+export const setGame = (game_id) => {
+  let data = {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
+  }
+
+  return dispatch => {
+    fetch(`${ baseUrl }/games/${game_id}`, data)
+      .then(response => response.json())
+      .then(game => dispatch({
+          type: 'SET_GAME',
+          payload: game
       }))
       .catch(err => err)
   }
