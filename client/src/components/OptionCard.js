f1b1848b6ff26c5c02d0a0b62aaa17cfec9f2b36
abourke09 <aabourke@gmail.com>
2019-05-31 15:12:57 -0700

    adds order to the OptionCard so that they always display in the order they were drawn

--- client/src/components/OptionCard.js
+++ client/src/components/OptionCard.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card'
 
 const OptionCard = (props) => {
     return (
-      <Card bg="light" style={ {margin: '25px', flex: '0 1 24%'} }>
+      <Card bg="light" style={ { order: `${props.option.draw}`,margin: '25px', flex: '0 1 24%'} }>
         <Card.Header>Draw #{props.option.draw}</Card.Header>
         <Card.Body>
           <Card.Subtitle>{props.option.player.name}: {props.option.description}</Card.Subtitle>
