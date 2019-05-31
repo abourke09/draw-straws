e113b6d765d89add2dcf2ac06cb9e860ff8fc355
abourke09 <aabourke@gmail.com>
2019-05-30 19:39:42 -0700

    The Option Card constant displays information about each draw, with details like the game description and the player's name.

---
+++ client/src/components/OptionCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+
+const OptionCard = (props) => {
+    return (
+      <Card bg="light" style={ {margin: '25px', flex: '0 1 24%'} }>
+        <Card.Header>Draw #{props.option.draw}</Card.Header>
+        <Card.Body>
+          <Card.Subtitle>{props.option.player.name}: {props.option.description}</Card.Subtitle>
+        </Card.Body>
+      </Card>
+      )
+}
+
+export default OptionCard;
