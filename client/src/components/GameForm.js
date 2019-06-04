eb3244ded95c3836b4afe1f384abfaca6ade311d
abourke09 <aabourke@gmail.com>
2019-06-03 17:31:59 -0700

    adds fisher-yates shuffle function and tests out shuffling a set array and displaying it on the DOM

--- client/src/components/GameForm.js
+++ client/src/components/GameForm.js
@@ -4,10 +4,22 @@ import Button from 'react-bootstrap/Button'
 
 class GameForm extends Component {
 
+// Fisher-Yates shuffle: https://javascript.info/task/shuffle
+  shuffleDraw(array) {
+  //  let array = ["Annie", "Grace", "Lindsay"];
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array[0]
+  }
+
+
   render() {
+    let myArray = ["Annie", "Grace", "Lindsay"]
     return(
       <div className="gameForm">
-        <h1 align= 'center'>Play A New Game</h1>
+        <h1 align= 'center'>Play A New Game: {this.shuffleDraw(myArray)}</h1>
         <Form style = { { margin: '0 auto', width: '300px' } }>
           <Form.Group controlId="exampleForm.ControlInput1">
             <Form.Label>Game Name:</Form.Label>
