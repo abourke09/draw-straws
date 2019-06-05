a2d47bfa08dd6302ec871c52d6fb853584c3ea14
abourke09 <aabourke@gmail.com>
2019-06-05 10:22:36 -0700

    creates newGameReducer to take in form data before it submits to the api database

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import playersReducer from './playersReducer'
 import gamesReducer from './gamesReducer'
 import optionsReducer from './optionsReducer'
+import newGameReducer from './newGameReducer'
 
 const rootReducer = combineReducers({
   players: playersReducer,
   games: gamesReducer,
-  options: optionsReducer
+  options: optionsReducer,
+  newGame: newGameReducer
 })
 
 export default rootReducer
---
+++ client/src/reducers/newGameReducer.js
@@ -0,0 +1,31 @@
+const initialState = {
+  //no ID for Game/Player/Option because nothing is saved to the database yet
+  name: "",
+  playersCount: 0,
+  players: [],
+  options: []
+}
+
+export default function newGameReducer(state = initialState, action) {
+  switch(action.type) {
+    // case 'FETCH_TODOS':
+    //   return { ...state, all: action.payload }
+    // case 'CREATE_TODO':
+    //   return { ...state, all: [...state.all, action.payload] }
+    // case 'DELETE_TODO':
+    //   return { ...state, all: state.all.filter(todo => todo.id !== action.payload.id) }
+    // case 'CREATE_SUB_TODO' :
+    //   let all = [ ...state.all ]
+    //   let idx = all.findIndex(todo => todo.id === action.payload.todo_id)
+    //   all[idx].sub_todos.push(action.payload)
+    //
+    //   return { ...state, all }
+    // case 'DELETE_SUB_TODO':
+    //   all = [ ...state.all ]
+    //   idx = all.findIndex(todo => todo.id === action.payload.todo_id)
+    //   all[idx].sub_todos = all[idx].sub_todos.filter(subTodo => subTodo.id !== action.payload.id)
+
+    //  return { ...state, all }
+    default: return state
+  }
+}
