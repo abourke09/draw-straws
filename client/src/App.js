dbf3f879290a0de9a090611f8ae68e75351b47ac
abourke09 <aabourke@gmail.com>
2019-06-06 15:29:18 -0700

    change GameForm to NewGame

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MyNavbar from './components/Navbar'
 import Home from './components/Home';
-import GameForm from './components/GameForm';
+import NewGame from './containers/NewGame';
 import Players from './containers/Players';
 import Games from './containers/Games';
 import PlayerShow from './containers/PlayerShow';
@@ -23,7 +23,7 @@ class App extends Component {
         <Router>
           <MyNavbar/>
           <Route exact path="/" component={Home}/>
-          <Route exact path="/new" component={GameForm}/>
+          <Route exact path="/new" component={NewGame}/>
           <Route exact path="/players" component={Players}/>
           <Route exact path="/games" component={Games}/>
           <Route path="/players/:id" component={PlayerShow} />
