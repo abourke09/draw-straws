19af387cc02877ee8f4f18846523740d6098a00c
abourke09 <aabourke@gmail.com>
2019-06-13 16:36:41 -0700

    removes unneeded variables and corrects the values of id, name, and onChange in the inputs

--- client/src/components/GameFormTwo.js
+++ client/src/components/GameFormTwo.js
@@ -14,27 +14,25 @@ class GameFormTwo extends Component {
       while (i <= (this.props.playersCount - 1)) {
         let player = this.props.playersArray[i]
         let option = this.props.optionsArray[i]
-        let idPlayer = `playersArray[${i}].name`
-        let idOption = "optionsArray[" + i + "].description"
 
         playersContainer.push(<div key={i.toString()}><label htmlFor="playersArray[i]">Player {i+1} Name:</label>
         <input
           className="form-control"
-          id={player.name}
-          name={idPlayer}
+          id={i}
+          name={player.name}
           type="text"
           value= {player.name}
-          onChange={this.props.handleChange}
+          onChange={this.props.handleChangePlayersInput}
         /></div>);
 
         optionsContainer.push(<div key={i}><label htmlFor="optionsArray[i]">Option {i+1}:</label>
         <input
           className="form-control"
-          id={idOption}
+          id={i}
           name={option.description}
           type="text"
           value= {option.description}
-          onChange={this.props.handleChange}
+          onChange={this.props.handleChangeOptionsInput}
         /></div>);
         i++;
       }
