d09e53a41d96c8f055ef236e7dafca075a48e5cc
abourke09 <aabourke@gmail.com>
2019-06-29 12:09:32 -0700

    makes keys more descriptive and removes unneeded keys

--- client/src/components/GameFormTwo.js
+++ client/src/components/GameFormTwo.js
@@ -15,20 +15,18 @@ class GameFormTwo extends Component {
         let player = this.props.playersArray[i]
         let option = this.props.optionsArray[i]
 
-        playersContainer.push(<div key={i.toString()}><label htmlFor="playersArray[i]">Player {i+1} Name:</label>
+        playersContainer.push(<div key={player.name}><label htmlFor="playersArray[i]">Player {i+1} Name:</label>
         <input
           className="form-control"
-          id={i}
           name={player.name}
           type="text"
           value= {player.name}
           onChange={this.props.handleChangePlayersInput}
         /></div>);
 
-        optionsContainer.push(<div key={i}><label htmlFor="optionsArray[i]">Option {i+1}:</label>
+        optionsContainer.push(<div key={option.description}><label htmlFor="optionsArray[i]">Option {i+1}:</label>
         <input
           className="form-control"
-          id={i}
           name={option.description}
           type="text"
           value= {option.description}
