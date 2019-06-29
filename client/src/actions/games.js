63541897b1ea4dcbcc206f3eba80d489cf048a8f
abourke09 <aabourke@gmail.com>
2019-06-29 12:22:58 -0700

    -builds out the createGame action to POST newly formated game data in JSON format to '.../api/games' -the dispatch send that game data to games_controller in the api to create the game on the backend -then the dispatch sends the game data to the reducers (twice), first to CREATE_GAME then second to CREATE_PLAYERS. This allows us to update the Redux store for both players and games

--- client/src/actions/games.js
+++ client/src/actions/games.js
@@ -41,3 +41,37 @@ export const setGame = (game_id) => {
       .catch(err => err)
   }
 }
+
+//create a new game
+export const createGame = gameState => {
+  //convert game data to a format that the game controller will like:
+  const game = {
+    name: gameState.gameName,
+    players: gameState.players
+  }
+
+  let data = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ game })
+  }
+
+  return dispatch => {
+    fetch(`${ baseUrl }/games`, data)
+      .then(response => response.json())
+      .then(formattedGame => dispatch({
+        type: 'CREATE_GAME',
+        payload: formattedGame
+      }))
+      .then(formattedGame => dispatch({
+        type: 'CREATE_PLAYERS',
+        payload: formattedGame.players
+      }))
+      .catch(err => err)
+  }
+
+
+}
