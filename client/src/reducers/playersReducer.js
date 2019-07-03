2e356734a6cccf0519a03b48ad9c056e1901fbad
abourke09 <aabourke@gmail.com>
2019-07-03 16:14:57 -0700

    -replaces CREATE_PLAYERS with CREATE_OR_UPDATE_PLAYER since we're now passing in one individual players instead of the entire array, and we're also making sure we don't duplicate players -new logic asks if the player's name being passed in is already in the redux store yet? If so, if finds that player's index and sets the redux store players index to that player's new data (action.payload). if not, it simply adds the new player to the array of players in the redux store

--- client/src/reducers/playersReducer.js
+++ client/src/reducers/playersReducer.js
@@ -9,9 +9,15 @@ export default function playersReducer(state = initialState, action) {
       return { ...state, all: action.payload }
     case 'SET_PLAYER':
       return { ...state, current: action.payload }
-    case 'CREATE_PLAYERS':
-    //i need to make an option to update the player if they've already been created
-      return { ...state, all: state.all.concat(action.payload)}
-  default: return state
+    case 'CREATE_OR_UPDATE_PLAYER':
+      if(state.all.find(p => p.name === action.payload.name)) {
+        let allArray = [ ...state.all ]
+        let idx = allArray.findIndex(player => player.name === action.payload.name)
+        allArray[idx] = action.payload
+        return { ...state, all: allArray }
+      } else {
+        return { ...state, all: state.all.concat(action.payload)}
+      }
+    default: return state
   }
 }
