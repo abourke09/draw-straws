3a562b8d02bca783c9cef2a7f1a4f22d7a240da8
abourke09 <aabourke@gmail.com>
2019-07-08 17:33:23 -0700

    Edits made to instructions

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,13 +3,14 @@ import React from 'react'
 const Home = () => (
   <div className="Home">
     <h1 align= 'center'>Welcome to Draw Straws!</h1>
-    <p>How to play: Draw Straws is a selection game that helps groups assign specific tasks to individuals.
-    To play, give the game a title and enter each player's name, then list each possible option for the game.
-    For example, a group of roommates could name their game House Chores and list options such as 1)Take out the trash,
-    2)Do the dishes, and 3)Vacuum the living room. Once that information is entered, hit the 'Draw Straws' button.
-    The game will then select a player at random, who will get the first choice to select an option.
-    The next randomly selected player will get to choose from the remaining options. This process repeats until
-    all of the players have chosen. The game is saved and can be reviewed in the All Games section.</p>
+    <p>Draw Straws is a selection game that helps groups assign specific tasks to individuals.</p>
+    <p>To play, select "New" from the menu and enter a game name and the number of players. Next, enter
+    each player's name and list every possible option for the game.</p>
+    <p><em>For example, a group of roommates could name their game House Chores and list options such as
+    1)Take out the trash, 2)Do the dishes, and 3)Vacuum the living room.</em></p>
+    <p>Once you click 'Next' to 'Draw Straws', a player will be selected at random to choose an option first.
+    This process repeats until all of the players have chosen. Lastly, the game is saved and can be reviewed
+    in the All Games section.</p>
   </div>
   )
 
