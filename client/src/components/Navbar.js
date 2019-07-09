f7cad7780974be66a55637e5afe14645d97dcb11
abourke09 <aabourke@gmail.com>
2019-07-08 17:43:54 -0700

    turns Navbar into a functional (Stateless) component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,20 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
-class myNavbar extends Component {
-  render() {
-    return (
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand>Draw Straws</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/new">New</Nav.Link>
-              <Nav.Link href="/players">All Players</Nav.Link>
-              <Nav.Link href="/games">All Games</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-    )
-  }
-}
+const myNavbar = () => (
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand>Draw Straws</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/new">New</Nav.Link>
+        <Nav.Link href="/players">All Players</Nav.Link>
+        <Nav.Link href="/games">All Games</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
 
 export default myNavbar;
