2dd45be2cb5dcae6701437b07a5357814f9c6e4a
abourke09 <aabourke@gmail.com>
2019-07-08 17:33:04 -0700

    optionsReducer and newGameReducer were never actually needed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,14 +2,10 @@ import { combineReducers } from 'redux'
 
 import playersReducer from './playersReducer'
 import gamesReducer from './gamesReducer'
-import optionsReducer from './optionsReducer'
-import newGameReducer from './newGameReducer'
 
 const rootReducer = combineReducers({
   players: playersReducer,
   games: gamesReducer,
-  options: optionsReducer,
-  newGame: newGameReducer
 })
 
 export default rootReducer
