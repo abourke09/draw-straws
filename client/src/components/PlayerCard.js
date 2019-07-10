3dc68b04914b04f04829a4aef6593021cdb96dcd
abourke09 <aabourke@gmail.com>
2019-07-10 10:34:32 -0700

    Project Review: adds 'Like' feature that increments a counter for each Player. Does not persist data in state.

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
 import { Link } from 'react-router-dom';
 
-const PlayerCard = (props) => {
-  const url = `/players/${props.player.id}`
+//js SIDEeffects setstate 
+class PlayerCard extends Component {
 
-  const gameNames = props.player.games.map( (game, index) => <ListGroup.Item key={index}>{game.name}</ListGroup.Item> )
+  constructor(props) {
+    super(props);
 
-    return (
-      <Card bg="light" style={ {margin: '25px', flex: '0 1 24%'} }>
-        <Card.Header>{props.player.name}</Card.Header>
+    this.state = {
+      counter: 0,
+    };
+  }
 
-        <Card.Body>
-          <Card.Subtitle>Number of Games Played: {props.player.games.length}</Card.Subtitle>
+  handleClick = event => {
+    this.setState({
+      ...this.state,
+      counter: this.state.counter + 1
+    })
+  }
 
-          <ListGroup className="mb-2 text-muted">{gameNames}</ListGroup>
+  render() {
+  const url = `/players/${this.props.player.id}`
 
-          <Link to= {url} >View Player's Page</Link>
-        </Card.Body>
+  const gameNames = this.props.player.games.map( (game, index) => <ListGroup.Item key={index}>{game.name}</ListGroup.Item> )
 
-      </Card>
-      )
+
+  return(
+    <Card bg="light" style={ {margin: '25px', flex: '0 1 24%'} }>
+      <Card.Header>{this.props.player.name}</Card.Header>
+
+      <Card.Body>
+        <Card.Subtitle>Number of Games Played: {this.props.player.games.length}</Card.Subtitle>
+
+        <ListGroup className="mb-2 text-muted">{gameNames}</ListGroup>
+
+        <Link to= {url} >View Player's Page</Link>
+        <p><button type="button" onClick={this.handleClick}>Like</button> : {this.state.counter}</p>
+
+      </Card.Body>
+
+    </Card>
+    )
+  }
 }
 
 export default PlayerCard;
