1dd61192e56222ae70e58453d9f708f2ee96f8e0
abourke09 <aabourke@gmail.com>
2019-07-10 08:38:28 -0700

    removes Nav, Navbar.Toggle, and Navbar.Collapse

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
 import { NavLink } from 'react-router-dom'
 
 const MyNavbar = () => (
   <div>
     <Navbar bg="light" expand="lg">
       <Navbar.Brand>Draw Straws</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <NavLink exact to="/">Home</NavLink>
-          <NavLink exact to="/new">New</NavLink>
-          <NavLink exact to="/players">All Players</NavLink>
-          <NavLink exact to="/games">All Games</NavLink>
-        </Nav>
-      </Navbar.Collapse>
+      <NavLink exact to="/"> Home | </NavLink>
+      <NavLink exact to="/new"> New | </NavLink>
+      <NavLink exact to="/players"> All Players | </NavLink>
+      <NavLink exact to="/games"> All Games </NavLink>
     </Navbar>
   </div>
 )
