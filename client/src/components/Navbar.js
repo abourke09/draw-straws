8a5d6c23952809200562504957d46bfb3a2581a8
abourke09 <aabourke@gmail.com>
2019-07-10 08:26:52 -0700

    Changes Nav.Link to NavLink to prevent the DOM from completely refreshing with each Nav Link click

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { NavLink } from 'react-router-dom'
 
-const myNavbar = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand>Draw Straws</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/new">New</Nav.Link>
-        <Nav.Link href="/players">All Players</Nav.Link>
-        <Nav.Link href="/games">All Games</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+const MyNavbar = () => (
+  <div>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>Draw Straws</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <NavLink exact to="/">Home</NavLink>
+          <NavLink exact to="/new">New</NavLink>
+          <NavLink exact to="/players">All Players</NavLink>
+          <NavLink exact to="/games">All Games</NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
 )
 
-export default myNavbar;
+export default MyNavbar;
